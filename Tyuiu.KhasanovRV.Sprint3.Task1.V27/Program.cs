6ed35a0cc30f71a01d4c4d2f6b34e66e49cf50ec
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-10-20 14:13:05 +0500

    Task1

---
+++ Tyuiu.KhasanovRV.Sprint3.Task1.V27/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhasanovRV.Sprint3.Task1.V27.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint3.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Хасанов Р. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Хасанов Ренат Владиславович | СМАРТб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение *");
+            Console.WriteLine("* ряда по формуле, при n=0,75                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double n = 0.75;
+            int startValue = 1, stopValue = 5;
+            Console.WriteLine("Переменная N = " + n);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда " + ds.GetMultiplySeries(n, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
