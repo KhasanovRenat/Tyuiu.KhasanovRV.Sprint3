36aca02cbd24d029ff2417c32f7fb647561f1908
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-10-26 11:08:56 +0500

    Task6

---
+++ Tyuiu.KhasanovRV.Sprint3.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KhasanovRV.Sprint3.Task6.V20.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint3.Task6.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 20, stopValue = 32;
+
+            var sp = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 41;
+
+            Assert.AreEqual(wait, sp);
+        }
+    }
+}
---
+++ Tyuiu.KhasanovRV.Sprint3.Task6.V20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhasanovRV.Sprint3.Task6.V20.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint3.Task6.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Хасанов Р. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Хасанов Ренат Владиславович | СМАРТб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [20, 32] количество всех делителей меньше 12          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 20, stopValue = 32;
+            
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
